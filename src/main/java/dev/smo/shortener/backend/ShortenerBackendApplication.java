ed52afa3c86efee0cd684e789116f13a6a1e9146
Simon Oser <simon.oser@gmail.com>
2025-10-14 11:11:43 +0200

    first commit

---
+++ src/main/java/dev/smo/shortener/backend/ShortenerBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.smo.shortener.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/smo/shortener/backend/ShortenerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.smo.shortener.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
